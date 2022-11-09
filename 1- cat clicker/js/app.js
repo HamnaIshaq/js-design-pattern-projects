1ad75dd41cd231f2d6e1dc4a4fa8a37fcc3c0a26
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-11-09 21:35:03 +0500

    Remove autio element

--- 1- cat clicker/js/app.js
+++ 1- cat clicker/js/app.js
@@ -1,6 +1,5 @@
 const cat = document.querySelector('.cat-img-container');
 const catClickedAmount = document.querySelector('.cat-clicked-amount');
-const audioMeow = document.querySelector('#audio-meow');
 
 cat.addEventListener('click', catClicked, false);
 
