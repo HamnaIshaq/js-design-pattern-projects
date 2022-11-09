6ca3b12c6aa3eadbf9a07a4ea4a7239760ba03c9
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-11-09 22:42:38 +0500

    Add cats on DOM from array of objects

--- 1- cat clicker/js/app.js
+++ 1- cat clicker/js/app.js
@@ -1,7 +1,37 @@
-const cat = document.querySelector('.cat-img-container');
+let catsImgContainer = document.querySelector('.cat-img-container');
+
+let catInfo = [
+  { 
+    name: 'Fluffy',
+    img: './assets/cat.png'
+  },
+  { 
+    name: 'Kitty',
+    img: './assets/cat.png'
+  }
+]
+
+for(let catNum = 0; catNum < catInfo.length; catNum++) {
+
+  let catContent = `
+    <p>${ catInfo[catNum].name }</p>
+    <div class="cat-img-wrapper">
+      <img class="cat-img" src="${ catInfo[catNum].img }" alt="cat">
+    </div>
+  `;
+
+  let catContainer = document.createElement('div');
+  catContainer.innerHTML = catContent;
+
+  catsImgContainer.appendChild(catContainer);
+}
+
+const cats = document.querySelectorAll('.cat-img-wrapper');
 const catClickedAmount = document.querySelector('.cat-clicked-amount');
 
-cat.addEventListener('click', catClicked, false);
+cats.forEach(cat => {
+  cat.addEventListener('click', catClicked, false);
+})
 
 function catClicked() {
   catClickedAmount.textContent = parseInt(catClickedAmount.textContent) + 1;
