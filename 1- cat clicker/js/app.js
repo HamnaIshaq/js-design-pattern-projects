74f898473edf9af985d176b9820c65e4659b73d0
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-11-11 00:25:52 +0500

    Add cats on nav select keeping track of total number of clicks of each individual cat

--- 1- cat clicker/js/app.js
+++ 1- cat clicker/js/app.js
@@ -1,41 +1,77 @@
 let catsImgContainer = document.querySelector('.cat-img-container');
+let catNavigation = document.querySelector('.navigation');
 
 let catInfo = [
   { 
     name: 'Fluffy',
-    img: './assets/cat-1.png'
+    img: './assets/cat-1.png',
+    clicked: 0
   },
   { 
     name: 'Kitty',
-    img: './assets/cat-1.png'
+    img: './assets/cat-2.png',
+    clicked: 0
+  },
+  { 
+    name: 'Martin',
+    img: './assets/cat-3.png',
+    clicked: 0
+  },
+  { 
+    name: 'Alexa',
+    img: './assets/cat-4.png',
+    clicked: 0
+  },
+  { 
+    name: 'Poppy',
+    img: './assets/cat-5.png',
+    clicked: 0
   }
 ]
 
 for(let catNum = 0; catNum < catInfo.length; catNum++) {
 
-  let catContent = `
-    <p>${ catInfo[catNum].name }</p>
-
-    <div class="cat-img-wrapper">
-      <img class="cat-img" src="${ catInfo[catNum].img }" alt="cat">
-    </div>
-
-    <p class="cat-clicked">Cat Clicked: <span class="cat-clicked-amount">0</span> </p>
+  let catList = `
+    <li style="border: 1px solid #000;">
+      <button>${catInfo[catNum].name}</button>
+    </li>
   `;
 
   let catContainer = document.createElement('div');
-  catContainer.innerHTML = catContent;
+  catContainer.innerHTML = catList;
 
-  catsImgContainer.appendChild(catContainer);
-}
+  catContainer.addEventListener('click', function() {
 
-const cats = document.querySelectorAll('.cat-img-wrapper');
+    catsImgContainer.innerHTML = '';
+    
+    let catContent = `
+      <p>${ catInfo[catNum].name }</p>
 
-cats.forEach(cat => {
-  cat.addEventListener('click', catClicked, false);
-})
+      <div class="cat-img-wrapper">
+        <img class="cat-img" src="${ catInfo[catNum].img }" alt="cat">
+      </div>
 
-function catClicked(e) {
-  let catClickedAmount = e.target.parentElement.parentElement.children[2].children[0];
-  catClickedAmount.textContent = parseInt(catClickedAmount.textContent) + 1;
+      <p class="cat-clicked">Cat Clicked: <span class="cat-clicked-amount">${catInfo[catNum].clicked}</span> </p>
+    `;
+
+    let catContainer = document.createElement('div');
+    catContainer.innerHTML = catContent;
+
+    catsImgContainer.appendChild(catContainer);
+
+    const cats = document.querySelectorAll('.cat-img-wrapper');
+
+    cats.forEach(cat => {
+      cat.addEventListener('click', catClicked, false);
+    })
+
+    function catClicked(e) {
+      let catClickedAmount = e.target.parentElement.parentElement.children[2].children[0];
+      catClickedAmount.textContent = parseInt(catClickedAmount.textContent) + 1;
+      catInfo[catNum].clicked = catClickedAmount.textContent;
+    }
+    
+  })
+
+  catNavigation.appendChild(catContainer);
 }
